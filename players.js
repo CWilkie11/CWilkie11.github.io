33542e33a1bd9605b166d100fed5723e61f77f55
CWilkie11 <coltan.wilkie@yahoo.com>
2026-03-01 11:38:14 -0500

    Create players.js

---
+++ players.js
@@ -0,0 +1,25 @@
+// NHL League Database
+const nhlData = {
+    teams: [
+        {
+            name: "Chicago Blackhawks",
+            id: "chi",
+            capSpace: 15000000,
+            roster: [
+                { id: 1, name: "Connor Bedard", pos: "C", salary: 950000, xG: 0.85 },
+                { id: 2, name: "Seth Jones", pos: "D", salary: 9500000, xG: 0.32 },
+                { id: 3, name: "Teuvo Teravainen", pos: "LW", salary: 5400000, xG: 0.58 }
+            ]
+        },
+        {
+            name: "Toronto Maple Leafs",
+            id: "tor",
+            capSpace: 500000,
+            roster: [
+                { id: 4, name: "Auston Matthews", pos: "C", salary: 13250000, xG: 0.95 },
+                { id: 5, name: "Mitch Marner", pos: "RW", salary: 10903000, xG: 0.72 },
+                { id: 6, name: "Matthew Knies", pos: "LW", salary: 925000, xG: 0.45 }
+            ]
+        }
+    ]
+};
